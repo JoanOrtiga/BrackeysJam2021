6b65ab10b821882841de44748e88a2bfec4e90f8
LadyMarina <49723271+LadyMarina@users.noreply.github.com>
2021-02-15 15:28:38 +0100

    PickUp with houme

--- SpaceTogether/Assets/Scripts/PickUpObject.cs
+++ SpaceTogether/Assets/Scripts/PickUpObject.cs
@@ -7,22 +7,16 @@ public class PickUpObject : MonoBehaviour
     public float pickUpDistance = 5;
     public LayerMask objectLayerMask;
     public GameObject handCenter;
-
+    public float leaveDistance = 3;
 
 
     GameObject objectPickUp;
     private bool onHand = false;
-
+    private Vector3 originalPosition;
+    private float timer;
 
     void Update()
     {
-        
-        if (onHand == true && Input.GetKeyDown("e"))
-        {
-            onHand = false;
-            objectPickUp.transform.SetParent(null);
-            objectPickUp.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
-        }
 
         Ray l_Ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.0f));
         RaycastHit l_RaycastHit;
@@ -30,18 +24,48 @@ public class PickUpObject : MonoBehaviour
 
         if (Physics.Raycast(l_Ray, out l_RaycastHit, pickUpDistance, objectLayerMask.value))
         {
-            print("Press E to pick Up");
+            
             if (Input.GetKeyDown("e") && onHand == false)
             {
+                print("Press E to pick Up");
                 onHand = true;
                 objectPickUp = l_RaycastHit.transform.gameObject;
+
+                originalPosition = objectPickUp.transform.position;
                 objectPickUp.transform.position = handCenter.transform.position;
                 objectPickUp.transform.SetParent(handCenter.transform);
                 objectPickUp.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-              
+
+                timer = 0;
             }
         }
 
+        if (onHand == true && Input.GetKeyDown("e") && (timer > 0.25))
+        {
+            float dist = Vector3.Distance(handCenter.transform.position, originalPosition);
+            print(dist);
+            
+
+            if (dist > leaveDistance || objectPickUp.GetComponent<ObjectPlace>().isStartPlace == false)
+            {
+                objectPickUp.transform.SetParent(null);
+                objectPickUp.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+                objectPickUp.GetComponent<ObjectPlace>().isStartPlace = false;
+                onHand = false;
+            }
+            else if(objectPickUp.GetComponent<ObjectPlace>().isStartPlace == true && dist < leaveDistance)
+            {
+                objectPickUp.transform.SetParent(null);
+                objectPickUp.transform.position = originalPosition;
+                objectPickUp.GetComponent<ObjectPlace>().isStartPlace = true;
+                onHand = false;
+
+            }
+            timer = 0;
+           
+        }
+
+        timer += Time.deltaTime;
 
     }
 
